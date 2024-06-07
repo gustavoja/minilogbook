a7f2f34d33a04457796804d6367734a4856e7c93
Gustavo Jiménez <712020:1636ed80-4d7e-41af-86d5-562701e315ad@bots.bitbucket.org>
2024-06-08 01:03:31 +0200

    Bind view model to repo and use cases

--- app/src/main/java/com/gjapps/minilogbook/ui/blodglucroserecords/BloodGlucoseRecordsViewModel.kt
+++ app/src/main/java/com/gjapps/minilogbook/ui/blodglucroserecords/BloodGlucoseRecordsViewModel.kt
@@ -1,20 +1,55 @@
 package com.gjapps.minilogbook.ui.blodglucroserecords
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.gjapps.minilogbook.data.models.BloodGlucoseUnit
+import com.gjapps.minilogbook.data.repositories.BloodGlucoseRepository
+import com.gjapps.minilogbook.domain.usecases.ConvertFloatToLocaleDecimalStringUseCase
+import com.gjapps.minilogbook.domain.usecases.ConvertLocaleDecimalStringFloatUseCase
+import com.gjapps.minilogbook.domain.usecases.DateToLocaleStringFormatUseCase
 import com.gjapps.minilogbook.domain.usecases.SanitizeDecimalNumberUseCase
+import com.gjapps.minilogbook.ui.blodglucroserecords.components.recordslist.uistates.BloodGlucoseRecordItemUIState
+import com.gjapps.minilogbook.ui.blodglucroserecords.components.recordslist.uistates.BloodGlucoseRecordsListUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class BloodGlucoseRecordsViewModel @Inject constructor(private val sanitizeDecimalNumberUseCase: SanitizeDecimalNumberUseCase) : ViewModel() {
+class BloodGlucoseRecordsViewModel @Inject constructor(private val bloodGlucoseRecordsRepository: BloodGlucoseRepository,
+                                                       private val sanitizeDecimalNumberUseCase: SanitizeDecimalNumberUseCase,
+                                                       private val dateToLocaleStringFormatUseCase: DateToLocaleStringFormatUseCase,
+                                                       private val convertLocaleDecimalStringFloatUseCase: ConvertLocaleDecimalStringFloatUseCase,
+                                                       private val convertFloatToLocaleDecimalStringUseCase: ConvertFloatToLocaleDecimalStringUseCase
+) : ViewModel() {
     private val _uiState: MutableStateFlow<BloodGlucoseRecordsUiState> = MutableStateFlow(
-        BloodGlucoseRecordsUiState("", "", BloodGlucoseUnit.Mgdl, recordsState =  RecordsState.Empty)
+        BloodGlucoseRecordsUiState("", "", BloodGlucoseUnit.Mgdl, recordsState =  BloodGlucoseRecordsListUIState.Empty)
     )
-    val uiState = _uiState.asStateFlow()
+    val  uiState = _uiState.asStateFlow()
+
+    private val bloodGlucoseRecordsListUiState = bloodGlucoseRecordsRepository
+        .bloodGlucoseRecords
+        .map{
+            val recordsUIStates = it.map { record -> BloodGlucoseRecordItemUIState(convertFloatToLocaleDecimalStringUseCase(record.value), dateToLocaleStringFormatUseCase(record.date)) }
+            if(it.any()) BloodGlucoseRecordsListUIState.WithBloodGlucoseRecords(recordsUIStates) else BloodGlucoseRecordsListUIState.Empty
+        }
+        .catch {
+            _uiState.update {
+                it.copy(recordsState = BloodGlucoseRecordsListUIState.Error)
+            }
+        }
+        .onEach { list ->
+            _uiState.update {
+                it.copy(recordsState = list)
+            }
+        }
 
     fun onRecordValueChanged(newValue: String) {
         val currentValue = _uiState.value.newRecordInputValue
@@ -30,8 +65,35 @@ class BloodGlucoseRecordsViewModel @Inject constructor(private val sanitizeDecim
     }
 
     fun onSaveRecordValue() {
+        val record = _uiState.value.newRecordInputValue
+        if(record.isEmpty())
+            return
+
+        addRecord(record)
         _uiState.update {
-            it.copy(newRecordInputValue = "", average = "", recordsState = RecordsState.WithRecords(listOf(it.newRecordInputValue)))
+            it.copy(newRecordInputValue = "")
+        }
+    }
+
+    fun onLoaded()
+    {
+        viewModelScope.launch(CoroutineExceptionHandler { _, exception ->
+            _uiState.update {
+                it.copy(recordsState = BloodGlucoseRecordsListUIState.Error)
+            }
+        }) {
+            bloodGlucoseRecordsListUiState.collect()
+        }
+    }
+
+    private fun addRecord(value: String){
+        viewModelScope.launch (CoroutineExceptionHandler{ _, exception ->
+            _uiState.update {
+                it.copy(recordsState = BloodGlucoseRecordsListUIState.Error)
+            }
+        }) {
+            bloodGlucoseRecordsRepository.saveRecord(convertLocaleDecimalStringFloatUseCase(value))
         }
     }
 }
+
