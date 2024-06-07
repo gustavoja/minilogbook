c48811b611369a13a464d81fa08a628a700826a7
Gustavo Jiménez <712020:1636ed80-4d7e-41af-86d5-562701e315ad@bots.bitbucket.org>
2024-06-07 14:10:23 +0200

    Add SanitizeDecimalNumberUseCase

---
+++ app/src/main/java/com/gjapps/minilogbook/domain/usecases/SanitizeDecimalNumberUseCase.kt
@@ -0,0 +1,27 @@
+package com.gjapps.minilogbook.domain.usecases
+
+import java.text.DecimalFormatSymbols
+
+interface SanitizeDecimalNumberUseCase {
+    operator fun invoke(currentValue:String,newValue: String): String
+}
+
+class SanitizeDecimalNumberUseCaseImpl : SanitizeDecimalNumberUseCase {
+    override fun invoke(currentValue: String, newValue: String): String {
+        val decimalSeparator = getDecimalSeparatorForCurrentLocale()
+        if(newValue.count { it == decimalSeparator } > 1)
+            return currentValue
+
+        return removeNonDigitsExceptDecimalSeparators(newValue)
+    }
+
+    fun removeNonDigitsExceptDecimalSeparators(input: String): String {
+        val decimalSeparator = getDecimalSeparatorForCurrentLocale()
+        return input.filter { it.isDigit() || it == decimalSeparator}
+    }
+
+    fun getDecimalSeparatorForCurrentLocale(): Char {
+        val symbols = DecimalFormatSymbols.getInstance()
+        return symbols.decimalSeparator
+    }
+}
