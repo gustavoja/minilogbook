1366fed1643276c512c254c767dadca5b3da75c8
Gustavo Jiménez <712020:1636ed80-4d7e-41af-86d5-562701e315ad@bots.bitbucket.org>
2024-06-08 11:47:53 +0200

    remove log

--- app/src/main/java/com/gjapps/minilogbook/domain/usecases/ConvertFloatToLocaleDecimalStringUseCase.kt
+++ app/src/main/java/com/gjapps/minilogbook/domain/usecases/ConvertFloatToLocaleDecimalStringUseCase.kt
@@ -10,10 +10,8 @@ interface ConvertFloatToLocaleDecimalStringUseCase {
 class ConvertFloatToLocaleDecimalStringUseCaseImpl: ConvertFloatToLocaleDecimalStringUseCase {
     override operator fun invoke(value: Float): String {
         val locale = Locale.getDefault()
-        println("value: $value")
         val decimalFormat = DecimalFormat.getInstance(locale)
         decimalFormat.maximumFractionDigits = 6
-        println("decimalFormat: $decimalFormat")
         return decimalFormat.format(value)
     }
 }
