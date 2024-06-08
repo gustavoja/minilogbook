cbfd61f331d58321fb7fdf9ebdcaa320cf915550
Gustavo Jiménez <712020:1636ed80-4d7e-41af-86d5-562701e315ad@bots.bitbucket.org>
2024-06-08 11:48:43 +0200

    Add move memory storage implementation

--- app/src/main/java/com/gjapps/minilogbook/data/datasources/storage/local/memory/MemoryStorage.kt
+++ app/src/main/java/com/gjapps/minilogbook/data/datasources/storage/local/memory/MemoryStorage.kt
@@ -1,5 +1,6 @@
-package com.gjapps.minilogbook.data.datasources
+package com.gjapps.minilogbook.data.datasources.storage.local.memory
 
+import com.gjapps.minilogbook.data.datasources.storage.StorageDataSource
 import com.gjapps.minilogbook.data.models.BloodGlucoseRecordModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -8,17 +9,11 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-interface StorageDataSource {
-    val bloodGlucoseAverage: StateFlow<Float>
-    val bloodGlucoseRecords: Flow<List<BloodGlucoseRecordModel>>
-    suspend fun saveRecord(value: BloodGlucoseRecordModel, newAverage:Float)
-    suspend fun reloadBloodGlucoseRecords()
-    suspend fun bloodGlucoseRecordsCount():Int
-}
-
-class MemoryStorageDataSource() :StorageDataSource {
+class MemoryStorage : StorageDataSource {
     private var records = mutableListOf<BloodGlucoseRecordModel>()
-    private val _bloodGlucoseRecords = MutableSharedFlow<List<BloodGlucoseRecordModel>>(extraBufferCapacity = 1)
+    private val _bloodGlucoseRecords = MutableSharedFlow<List<BloodGlucoseRecordModel>>(extraBufferCapacity = 1,replay = 1)
+    private var _bloodGlucoseRecordsSum = 0f
+
     override val bloodGlucoseRecords: Flow<List<BloodGlucoseRecordModel>>
         get() = _bloodGlucoseRecords.asSharedFlow()
 
@@ -26,8 +21,9 @@ class MemoryStorageDataSource() :StorageDataSource {
     override val bloodGlucoseAverage: StateFlow<Float>
         get() = _bloodGlucoseAverage.asStateFlow()
 
-    override suspend fun saveRecord(value: BloodGlucoseRecordModel, newAverage:Float) {
+    override suspend fun saveRecord(value: BloodGlucoseRecordModel, newAverage:Float, newRecordsSum:Float) {
         records.add(value)
+        _bloodGlucoseRecordsSum = newRecordsSum
         _bloodGlucoseRecords.emit(records)
         _bloodGlucoseAverage.emit(newAverage)
     }
@@ -37,5 +33,9 @@ class MemoryStorageDataSource() :StorageDataSource {
 
     override suspend fun bloodGlucoseRecordsCount():Int{
         return records.count()
+    }
+
+    override suspend fun getBloodGlucoseRecordsSum(): Float {
+        return  _bloodGlucoseRecordsSum
     }
 }
